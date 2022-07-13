49c1b6110e1a2757f17f995e3cdf1f58653523c3
PHUONG.DD194141@sis.hust.edu.vn <PHUONG.DD194141@sis.hust.edu.vn>
2022-07-14 01:38:34 +0700

    img + home

--- view/home.php
+++ view/home.php
@@ -2,25 +2,31 @@
 <?php include_once("layout.php") ?>
 <html>
     <body>
-    <div class="container">
-    <table>
-        <thead>
-            <tr><th style="border: solid">Table</th></tr>
-        </thead>
-        <tbody>
-            <?php
-                $sql = "SELECT * FROM ban";
-                $result = $connect->query($sql);
-                while($row = $result->fetch_assoc()) {
-             ?>
-            <form method="POST">
-                 <tr>
-                    <td style="border: solid"><?php echo $row["SO_BAN"] ?></td>
-                </tr>
-            </form>
-            <?php } ?>
-        </tbody>
-    </table>
-    </div>
+        <div class="container">
+            <table>
+                <thead>
+                    <tr><th style="border: solid">Table</th></tr>
+                </thead>
+                <tbody>
+                    <!-- <?php
+                        $sql = "SELECT * FROM ban";
+                        $result = $connect->query($sql);
+                        while($row = $result->fetch_assoc()) {
+                    ?>
+                    <form method="POST">
+                        <tr>
+                            <td style="border: solid"><?php echo $row["SO_BAN"] ?></td>
+                        </tr>
+                    </form>
+                    <?php } ?> -->
+                </tbody>
+            </table>
+
+            <!-- Danh sách bàn -->
+            <!-- Hiển thị bàn và trạng thái -->
+            
+            <!-- Hoa don -->
+            <!-- Gồm các món, giá, tổng tiền, Khách hàng -->
+        </div>
     </body>
 </html>
