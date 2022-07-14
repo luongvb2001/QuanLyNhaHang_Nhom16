ccc80bfdd7edee9a02badcff6b980c02dff988c8
LuongVB <luonghc2001@gmail.com>
2022-07-14 17:19:08 +0700

    update

--- view/Security/register_Success.php
+++ view/Security/register_Success.php
@@ -1,3 +1,6 @@
+<?php
+    require_once('../../controller/Security/auth.php');
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,7 +10,7 @@
     </head>
     <body>
         <div class="form">
-            <p class="message">Đăng ký thành công <a href="register.php">Đăng nhập lại</a></p>
+            <p class="message">Đăng ký thành công <a href="login.php">Đăng nhập lại</a></p>
 </div>
     </body>
 </html>
