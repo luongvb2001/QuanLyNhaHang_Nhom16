8ddac28171d96801bc12e17bb7135e6f7c98dc4e
LuongVB <luonghc2001@gmail.com>
2022-07-16 14:54:07 +0700

    update

--- view/home.php
+++ view/home.php
@@ -2,65 +2,50 @@
     require_once('../controller/auth.php');
 ?>
 <?php include_once("../model/db.php") ?>
-<!-- <?php include_once("layout.php") ?> -->
+<?php include_once("layout.php") ?>
 <html>
     <head>
         <link rel="stylesheet" href="../css/home.css">
     </head>
 
     <body>
-        <div class="container-home">
-        <h1>Phuong Do</h1>
-            <!-- Danh sách bàn -->
-            <!-- Hiển thị bàn và trạng thái 0 là rảnh 1 là bận-->
-            <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </nav>
-            <div class="table-home">
-                <form method="POST">
-                    <?php
-                        $sql = "SELECT * FROM ban";
-                        $result = $connect->query($sql);
-                        while($row = $result->fetch_assoc()) {
-                    ?>
-                    <button>
-                            <img src="../images/tabkebusy.png" alt="">
-                        <?php 
-                            if($row["TRANG_THAI"] == 0){
-                                echo'<img src="../images/tick-xanh.png" alt="">';
-                            }
-                            else if($row["TRANG_THAI"] == 1){
-                                echo'<img src="../images/tick-red.png" alt="">';
-                            }
-                        ?>
-                        <!-- <img src="../images/tick-red.png" alt=""> -->
-                        <!-- <img src="../images/tick-xanh.png" alt=""> -->
-                    </button>                  
-                    <?php } ?>
-                </form>
+
+    <div class="container">
+        <div id="container_1">
+            <div>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <button class="nav-link active"  data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Danh Sách Bàn</button>
+                    <button class="nav-link" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Menu</button>
+                </div>
             </div>
-            <!-- Hoa don -->
-            <!-- Gồm các món, giá, tổng tiền, Khách hàng -->
-            <div class="add-mon-home">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Mon an</th>
-                            <th>So luong</th>
-                            <th>Don  gia</th>
-                        </tr>
-                    </thead>
-                </table>
-                <button>Tong tien</button>
+            <div class="tab-content" id="nav-tabContent">
+                <!-- Content Danh sách bàn -->
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <div style="height: 630px; overflow:auto;" class="btn_matrix" role="group" aria-label="Three Column Button Matrix">
+                        <form action="" method="post">
+                            <?php
+                                $sql = "SELECT * FROM ban";
+                                $result = $connect->query($sql);
+                                while($row = $result->fetch_assoc()) {
+                            ?>
+                            <button id="ban<?php echo $row["SO_BAN"] ?>" style="margin:5px; " type="submit" class="btn btn-outline-primary btn_ban" value='<?php echo $row["SO_BAN"] ?>' name = "choose_ban">
+                                <p style= "float: left; margin: 0;font-weight: bold">  <?php echo $row["SO_BAN"] ?> </p>
+                                <?php 
+                                    if($row["TRANG_THAI"] == 0){
+                                        echo'<img style="width:30%; float: right " src="../images/tick-xanh.png" alt="Table" >';
+                                    }
+                                    else if($row["TRANG_THAI"] == 1){
+                                        echo'<img style="width:30%; float: right " src="../images/tick-yellow.png" alt="Table" >';
+                                    }
+                                ?>
+                                <img style="width:84px " src="../images/tablebusy.png" alt="Table" >
+                            </button>
+                            <?php } ?>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <!-- Thanh toan -->
-            <div class="hoa-don-home">
-                <button>Thanh toan</button>
-            </div>
-            
         </div>
+    </div>
     </body>
 </html>
