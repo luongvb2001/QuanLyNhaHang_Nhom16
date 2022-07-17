381e53dab9749754bf233cb08bc3fff780f29e77
phamdinhminh030801@gmail.coms <phamdinhminh030801@gmail.com>
2022-07-17 20:05:17 +0700

    add lần 3

--- js/setting.js
+++ js/setting.js
@@ -1,5 +1,14 @@
 function show_Edit_MonAn(){
     document.getElementById('table_edit_monan').style.display="table";
+    document.getElementById('get_member').style.display="none";
+}
+function showUpdate_member(){
+    document.getElementById('get_member').style.display="inline";
+    document.getElementById('table_edit_monan').style.display="none";
+    document.getElementById('mon_phu').style.display = "none";
+    document.getElementById('mon_chinh').style.display = "none";
+    document.getElementById('do_uong').style.display = "none";
+    document.getElementById('hoa_qua').style.display = "none";
 }
 function showTable1(){
     document.getElementById('mon_phu').style.display = "table";
